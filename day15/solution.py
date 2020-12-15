d8608076f4a64152e89c73d62f1e481b01ce26ab
Junqiang Qian <junqiang.qian@hotmail.co.uk>
2020-12-15 19:03:53 +0000

    refactor - using less memory

--- day15/solution.py
+++ day15/solution.py
@@ -1,37 +1,28 @@
 class Solution:
 
-    def __init__(self):
-        # self.input = [20,9,11,0,1,2]
-        self.input = [20,9,11,0,1,2]
-
-    def solve_part_1(self, N):
+    def solve(self, input, N):
         spoken_map, last_spoken = {}, None
-        for i in range(len(self.input)):
-            spoken_map[self.input[i]] = [i + 1]
-            last_spoken = self.input[i]
-        n = len(self.input)
+        for i in range(len(input)):
+            spoken_map[input[i]] = i + 1,
+        n = len(input)
+        last_spoken = input[n - 1]
         while n < N:
+            n += 1
             if len(spoken_map[last_spoken]) == 1:
                 # new number
                 last_spoken = 0
-                if last_spoken in spoken_map:
-                    spoken_map[0].append(n + 1)
-                else:
-                    spoken_map[0] = [n + 1] 
             else:
-                last_spoken = spoken_map[last_spoken][-1] - spoken_map[last_spoken][-2]
-                if last_spoken in spoken_map:
-                    spoken_map[last_spoken].append(n + 1)
-                else:
-                    spoken_map[last_spoken] = [n + 1]
-            n += 1
+                last_spoken = spoken_map[last_spoken][1] - spoken_map[last_spoken][0]
+            if last_spoken not in spoken_map:
+                spoken_map[last_spoken] = n,
+            elif len(spoken_map[last_spoken]) == 1:
+                spoken_map[last_spoken] = spoken_map[last_spoken][0], n
+            else:
+                spoken_map[last_spoken] = spoken_map[last_spoken][1], n
         return last_spoken
-
-    def solve_part_2(self):
-        return self.solve_part_1(30000000)
     
-
 if __name__ == '__main__':
     s = Solution()
-    print(s.solve_part_1(2020))
-    print(s.solve_part_2())
+    input = [20,9,11,0,1,2]
+    print(s.solve(input, 2020))
+    print(s.solve(input, 30000000))
