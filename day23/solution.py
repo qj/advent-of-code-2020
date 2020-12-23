ffca5cda7ce7994539965e9c9a051bc22e4f28c6
Junqiang Qian <junqiang.qian@hotmail.co.uk>
2020-12-23 16:49:57 +0000

    cleanup

--- day23/solution.py
+++ day23/solution.py
@@ -11,10 +11,6 @@ class Cup:
         return f"Cup {self.label}"
 
 class Solution:
-
-    def __init__(self, tdata: bool):
-        self.input = "389125467" if tdata else "562893147"
-        self.input = [int(i) for i in self.input]
     
     def calc_destination(self, n):
         if n == 1:
@@ -22,32 +18,30 @@ class Solution:
         else:
             return n - 1
 
-    def solve_part_1(self):
-        iterations, curr, length = 0, 0, len(self.input)
+    def solve_part_1(self, tdata: bool):
+        input = "389125467" if tdata else "562893147"
+        input = [int(i) for i in input]
+        iterations, curr, length = 0, 0, len(input)
 
         while iterations < 100:
-            current = self.input[curr]
+            current = input[curr]
             removed_indices = [(curr + 1) % length, (curr + 2) % length, (curr + 3) % length]
-            three_adj = [self.input[(curr + 1) % length], self.input[(curr + 2) % length], self.input[(curr + 3) % length]]
+            three_adj = [input[(curr + 1) % length], input[(curr + 2) % length], input[(curr + 3) % length]]
             target = self.calc_destination(current)
-            while self.input.index(target) in removed_indices:
+            while input.index(target) in removed_indices:
                 target = self.calc_destination(target)
-            self.input = [x for x in self.input if x not in three_adj]
+            input = [x for x in input if x not in three_adj]
             for i, c in enumerate(three_adj):
-                self.input.insert(self.input.index(target) + i + 1, c)
-            curr = (self.input.index(current) + 1) % length
+                input.insert(input.index(target) + i + 1, c)
+            curr = (input.index(current) + 1) % length
             iterations += 1
         
-        index_of_one = self.input.index(1)
+        index_of_one = input.index(1)
 
-        return "".join(str(c) for c in self.input[(index_of_one + 1) % length:] + self.input[:index_of_one])
+        return "".join(str(c) for c in input[(index_of_one + 1) % length:] + input[:index_of_one])
 
     def solve_part_2(self, num_cups: int, num_iterations: int, tdata: bool):
-        if tdata:
-            input = "389125467"
-        else:
-            input = "562893147"
-
+        input = "389125467" if tdata else "562893147"
         initial_labels = [int(i) for i in input]
         
         cups = {}
@@ -82,6 +76,6 @@ class Solution:
 
         
 if __name__ == '__main__':
-    s = Solution(True)
-    print(s.solve_part_1())
+    s = Solution()
+    print(s.solve_part_1(False))
     print(s.solve_part_2(1000000, 10000000, False))
